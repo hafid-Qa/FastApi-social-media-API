a2f3033fe428356b51ac417c1a5360fb0a208cfa
Hafid Qarchi <haqarchi@gmail.com>
2023-01-22 23:20:09 +0900

    post request body logging in fastapi

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -11,3 +12,9 @@ async def root():
 @app.get("/posts")
 def get_posts():
     return {"data": "bunch of posts"}
+
+
+@app.post("/posts/new")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"message": "post created successfully"}
