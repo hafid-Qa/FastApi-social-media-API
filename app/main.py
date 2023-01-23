49ff00795b177969135004b24f3de724bc2c9cb5
Hafid Qarchi <haqarchi@gmail.com>
2023-01-23 22:25:53 +0900

    db connetion setup in main file

--- app/main.py
+++ app/main.py
@@ -6,8 +6,19 @@ from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from . import models
+from .database import SessionLocal, engine
 
+models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 class Post(BaseModel):
