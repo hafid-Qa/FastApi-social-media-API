da6766ff8f003bcd4bcd67c07616f06aaadb8071
Hafid Qarchi <haqarchi@gmail.com>
2023-01-23 14:42:35 +0900

    get one post

--- main.py
+++ main.py
@@ -18,6 +18,12 @@ class Post(BaseModel):
 my_posts = [{"id": 1, "title": "post 1", "content": "content 1"}, {"id": 2, "title": "post 2", "content": "content 2"}]
 
 
+def find_post(id):
+    for post in my_posts:
+        if post["id"] == id:
+            return post
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello Wor"}
@@ -35,3 +41,9 @@ def create_post(post: Post):
     post_dict["id"] = randrange(0, 100000)
     my_posts.append(post_dict)
     return {"data": post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):  # perform validation and convert id to int at the same time
+    post = find_post(id)
+    return {"data": post}
