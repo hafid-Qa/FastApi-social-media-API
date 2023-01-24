fa08fd5e80a9fe3f148558a46105b5d4b5347a37
Hafid Qarchi <haqarchi@gmail.com>
2023-01-24 18:40:48 +0900

    user schema validation

--- app/main.py
+++ app/main.py
@@ -75,3 +75,11 @@ def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
     post_query.update(post.dict(), synchronize_session=False)
     db.commit()
     return post_query.first()
+
+
+@app.post("/users", status_code=status.HTTP_201_CREATED)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
