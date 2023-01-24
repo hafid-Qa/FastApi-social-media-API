9af0efe96e204e3721f16a80f5b38476f0504cd3
Hafid Qarchi <haqarchi@gmail.com>
2023-01-24 14:48:24 +0900

    delete a post

--- app/main.py
+++ app/main.py
@@ -45,7 +45,6 @@ def get_posts(db: Session = Depends(get_db)):
     return {"data": posts}
 
 
-# title str, content str
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post, db: Session = Depends(get_db)):
     new_post = models.Post(**post.dict())
@@ -64,13 +63,12 @@ def get_post(id: int, db: Session = Depends(get_db)):
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts WHERE id= %s RETURNING *""", (str(id),))
-    deleted_post = cursor.fetchone()
-    conn.commit()
-    if deleted_post == None:
+def delete_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id)
+    if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} Not Found")
-
+    post.delete(synchronize_session=False)
+    db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
