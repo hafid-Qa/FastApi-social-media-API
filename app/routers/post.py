08de52872065ee58263aa50af3518f04587116c6
Hafid Qarchi <haqarchi@gmail.com>
2023-01-24 20:17:35 +0900

    group requests into categories for better docs

--- app/routers/post.py
+++ app/routers/post.py
@@ -4,7 +4,7 @@ from .. import models, schemas
 from sqlalchemy.orm import Session
 from ..database import get_db
 
-router = APIRouter(prefix="/posts")
+router = APIRouter(prefix="/posts", tags=["Posts"])
 
 
 @router.get("/", response_model=List[schemas.PostResponse])
--- app/routers/user.py
+++ app/routers/user.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import Session
 from ..database import engine, get_db
 from .. import models, schemas, utils
 
-router = APIRouter(prefix="/users")
+router = APIRouter(prefix="/users",
+tags=["Users"])
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
