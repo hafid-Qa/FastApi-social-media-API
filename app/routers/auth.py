89a422fce12e75a3c6bbd8b7f4f73e5c192804c5
Hafid Qarchi <haqarchi@gmail.com>
2023-01-25 14:49:12 +0900

    validate user login response

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,3 +1,4 @@
+from urllib import response
 from fastapi import APIRouter, Depends, status, Response, HTTPException
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
@@ -6,7 +7,7 @@ from .. import database, schemas, models, utils, oauth2
 router = APIRouter(tags=["Authentication"])
 
 
-@router.post("/login")
+@router.post("/login", response_model=schemas.Token)
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     # Oauth2PasswordRequestForm returns 2 values: {"username": "","password":""}
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
