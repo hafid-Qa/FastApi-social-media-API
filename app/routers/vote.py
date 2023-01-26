40ea2ca2c7cb3cf45ab1899ba0f510d685e1adb2
Hafid Qarchi <haqarchi@gmail.com>
2023-01-26 21:22:38 +0900

    raise error if user already voted for the post

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -17,6 +17,11 @@ def vote(
     )
     found_vote = vote_query.first()
     if vote.dir == 1:
+        if found_vote:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f"User {current_user.id} already has voted for post {vote.post_id}",
+            )
 
     else:
         print("Vote")
