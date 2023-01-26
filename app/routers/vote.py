795802b125b735e755ecad9ee3bd5de7209af43f
Hafid Qarchi <haqarchi@gmail.com>
2023-01-26 21:26:22 +0900

    create vote if does not exits

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -22,6 +22,9 @@ def vote(
                 status_code=status.HTTP_409_CONFLICT,
                 detail=f"User {current_user.id} already has voted for post {vote.post_id}",
             )
-
+        new_vote = models.Vote(post_id=vote.post_id, user_id=current_user.id)
+        db.add(new_vote)
+        db.commit()
+        return {"message": f"successfully voted for post {vote.post_id}"}
     else:
         print("Vote")
