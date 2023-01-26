65b5a733d15bd0d363712e7671b1618d1f3ebcd6
Hafid Qarchi <haqarchi@gmail.com>
2023-01-26 21:19:05 +0900

    query db for vote with same post id and current user

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
+from sqlalchemy.orm import Session
+from .. import models, database, schemas, oauth2
+from ..database import get_db
 
 router = APIRouter(prefix="/vote", tags=["Vote"])
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def vote():
-    print("Vote")
+def vote(
+    vote: schemas.Vote,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(oauth2.get_current_user),
+):
+    vote_query = db.query(models.Vote).filter(
+        models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id
+    )
+    found_vote = vote_query.first()
+    if vote.dir == 1:
+
+    else:
+        print("Vote")
