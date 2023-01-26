6557236114ec54db871b4f80ac9c46c6f880492e
Hafid Qarchi <haqarchi@gmail.com>
2023-01-26 15:27:57 +0900

    add user id when creating post

--- app/routers/post.py
+++ app/routers/post.py
@@ -18,8 +18,7 @@ def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.
 def create_post(
     post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)
 ):
-
-    new_post = models.Post(**post.dict())
+    new_post = models.Post(user_id=current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
