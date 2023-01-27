226fc0cb817a0018a6e67a312145ea374a35a535
Hafid Qarchi <haqarchi@gmail.com>
2023-01-27 22:34:04 +0900

    auto generate migration

--- app/models.py
+++ app/models.py
@@ -13,6 +13,7 @@ class User(Base):
     id = Column(Integer, nullable=False, primary_key=True)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
+    phone_number = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
     updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"), onupdate=text("now()"))
 
