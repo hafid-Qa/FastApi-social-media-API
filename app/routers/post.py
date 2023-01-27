245da48fba2b1064741607076e1df0321d75c8db
Hafid Qarchi <haqarchi@gmail.com>
2023-01-27 20:37:24 +0900

    return posts with vote and ability to filter based on params

--- app/routers/post.py
+++ app/routers/post.py
@@ -18,17 +18,22 @@ def get_posts(
     search: Optional[str] = "",
 ):
     # returns all posts regardless of current user
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
-    # to return all posts created by a current user
-    # posts = db.query(models.Post).filter(models.Post.user_id == current_user.id).all()
-    results = (
+    # posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+
+    # returns all posts regardless of current user including vote
+    posts = (
         db.query(models.Post, func.count(models.Vote.post_id).label("votes"))
         .join(models.Vote, models.Post.id == models.Vote.post_id, isouter=True)
         .group_by(models.Post.id)
+        .filter(models.Post.title.contains(search))
+        .limit(limit)
+        .offset(skip)
         .all()
     )
+    # to return all posts created by a current user
+    # posts = db.query(models.Post).filter(models.Post.user_id == current_user.id).all()
     # print(results)
-    return results
+    return posts
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
