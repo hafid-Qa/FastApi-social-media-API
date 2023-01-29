d344d5e3e2874ca3f0d8273b429f2afd951e34bf
Hafid Qarchi <haqarchi@gmail.com>
2023-01-29 23:19:20 +0900

    create user test

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,9 +1,19 @@
 from fastapi.testclient import TestClient
 from app.main import app
+from app import schemas
 
 client = TestClient(app)
 
 
 def test_root():
+    print("Testing Root")
     res = client.get("/")
     assert res.json() == "to see documentation open: http://localhost:8000/docs or /redoc"
+
+
+def test_create_user():
+    res = client.post("/users/", json={"email": "test@example.com", "password": "testpassword", "phone_number": "01"})
+
+    new_user = schemas.UserResponse(**res.json())
+    assert res.status_code == 201
+    assert new_user.email == "test@example.com"
