486d9fdf1469d0774af80f6f4def773bc7a0b315
Hafid Qarchi <haqarchi@gmail.com>
2023-01-30 14:53:03 +0900

    get all posts test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 from fastapi.testclient import TestClient
 from app.main import app
 import pytest
-from app import schemas, models
+from app import models
+from app.oauth2 import create_access_token
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.config import settings
 from app.database import get_db
@@ -49,3 +49,14 @@ def test_user(client):
     new_user = res.json()
     new_user["password"] = user_data["password"]
     return new_user
+
+
+@pytest.fixture
+def token(test_user):
+    return create_access_token({"user_id": test_user["id"]})
+
+
+@pytest.fixture
+def authorized_client(client, token):
+    client.headers = {**client.headers, "Authorization": f"Bearer {token}"}
+    return client
---
+++ tests/test_posts.py
@@ -0,0 +1,4 @@
+def test_gel_all_posts(authorized_client):
+    res = authorized_client.get("/posts/")
+    print(res.json())
+    assert res.status_code == 200
