eab9eba139c1ed59ad18c37961cad79bb4e7a802
Hafid Qarchi <haqarchi@gmail.com>
2023-01-30 15:23:14 +0900

    validate all posts on test

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,4 +1,11 @@
-def test_gel_all_posts(authorized_client):
+from typing import List, Optional
+from app import schemas
+
+
+def test_gel_all_posts(authorized_client, test_posts):
     res = authorized_client.get("/posts/")
-    print(res.json())
+    assert len(res.json()) == len(test_posts)
+    posts = res.json()
+    for post in posts:
+        schemas.PostWithVoteResponse(**post)
     assert res.status_code == 200
