2e4d8fe453d1cf62e927acccca0f159ef2618573
Hafid Qarchi <haqarchi@gmail.com>
2023-01-30 15:33:41 +0900

    test get one post for not authenticated user

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -14,3 +14,12 @@ def test_gel_all_posts(authorized_client, test_posts):
 def test_unauthorized_user_get_all_posts(client, test_posts):
     res = client.get("/posts/")
     assert res.status_code == 401
+    assert res.json().get("detail") == "Not authenticated"
+
+
+def test_unauthorized_user_get_one_posts(client, test_posts):
+    res = client.get(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+    assert res.json().get("detail") == "Not authenticated"
+
+
