6274e8acc72614bfd4a582c5aae6cb6c6edafff3
Hafid Qarchi <haqarchi@gmail.com>
2023-01-30 13:08:05 +0900

    clear db before running the tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,11 +1,12 @@
 from fastapi.testclient import TestClient
 from app.main import app
-from app import schemas
+import pytest
+from app import schemas, models
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.config import settings
-from app.database import get_db, Base
+from app.database import get_db
 
 
 SQLALCHEMY_DATABASE_URL = (
@@ -16,7 +17,7 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base.metadata.create_all(bind=engine)
+
 # Dependency
 def override_get_db():
     db = TestingSessionLocal()
@@ -28,16 +29,21 @@ def override_get_db():
 
 app.dependency_overrides[get_db] = override_get_db
 
-client = TestClient(app)
+
+@pytest.fixture
+def client():
+    models.Base.metadata.drop_all(bind=engine)
+    models.Base.metadata.create_all(bind=engine)
+    yield TestClient(app)
 
 
-def test_root():
+def test_root(client):
     print("Testing Root")
     res = client.get("/")
     assert res.json() == "to see documentation open: http://localhost:8000/docs or /redoc"
 
 
-def test_create_user():
+def test_create_user(client):
     res = client.post("/users/", json={"email": "test@example.com", "password": "testpassword", "phone_number": "01"})
 
     new_user = schemas.UserResponse(**res.json())
