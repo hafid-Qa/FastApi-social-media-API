ef91445ade2f86201a3c867bad54374306bf87f7
Hafid Qarchi <haqarchi@gmail.com>
2023-01-30 14:05:12 +0900

    login user test

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,19 @@
 from app import schemas
+from jose import jwt
 from .database import client, session
+import pytest
+from app.config import settings
+
+
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "test@example.com", "password": "testpassword", "phone_number": "01"}
+    res = client.post("/users/", json=user_data)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user["password"] = user_data["password"]
+    return new_user
+
 
 def test_root(client):
     print("Testing Root")
@@ -13,3 +27,13 @@ def test_create_user(client):
     new_user = schemas.UserResponse(**res.json())
     assert res.status_code == 201
     assert new_user.email == "test@example.com"
+
+
+def test_login_user(client, test_user):
+    res = client.post("/login", data={"username": test_user["email"], "password": test_user["password"]})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get("user_id")
+    assert id == test_user["id"]
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200
